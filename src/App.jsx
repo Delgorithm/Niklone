c290f716f7b30f6116752b9a70ce1d47d13e1f4a
Delgorithm <delgorithm@gmail.com>
2024-03-28 18:56:40 +0100

    Feature : New pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Cart from './pages/Cart'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import User from './pages/User'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={ <Home />} />
+        <Route path="/cart" element={ <Cart />} />
+        <Route path="/user" element={ <User />} />
       </Routes>
     </Router>
   )
