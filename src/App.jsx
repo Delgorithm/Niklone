afd7705464250ef5876e227af0fc72a379774b21
Delgorithm <delgorithm@gmail.com>
2024-03-28 18:00:50 +0100

    Feature : Started to work on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './pages/Home'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Home />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Home />} />
+      </Routes>
+    </Router>
   )
 }
 
