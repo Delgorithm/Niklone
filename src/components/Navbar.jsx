41af56cbf60677c8b64d102b77a40d5ffaf0aa59
Delgorithm <delgorithm@gmail.com>
2024-03-28 18:01:00 +0100

    Added some pictures

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+import { CiMenuBurger } from "react-icons/ci";
+
+
+const Navbar = () => {
+  return (
+    <header>
+        <article>
+        </article>
+    </header>
+  )
+}
+
+export default Navbar
