cd0e533e6199df8d765c34d097a52038176f97ed
Delgorithm <delgorithm@gmail.com>
2024-04-01 16:09:58 +0200

    Feature : Carousel displayed correctly. Will need some adjustement

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import carouselShoes from "../assets/data/data.json";
+
+const Carousel = () => {
+    const { firstShoe, secondShoe, thirdShoe, fourthShoe, fifthShoe } = carouselShoes.carouselShoes;
+
+    const shoes = [firstShoe, secondShoe, thirdShoe, fourthShoe, fifthShoe];
+
+    return (
+        <article className='flex items-center overflow-x-auto whitespace-nowrap scroll-smooth overflow-hidden'>
+            {shoes.map((shoe, index) => (
+                <section key={index} className='m-6 cursor-pointer hover:scale-105 ease-in-out duration-150'>
+                    <figure>
+                        <img src={shoe.img} alt={shoe.alt} />
+                    </figure>
+                    <p className='text-sm'>{shoe.model}</p>
+                    <p className='text-sm text-gray-400'>{shoe.type}</p>
+                    <p className='text-sm mt-2 font-medium'>{shoe.price} €</p>
+                </section>
+            ))}
+        </article>
+    )
+}
+
+export default Carousel
