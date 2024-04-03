876c8cf1a8416ac7b9591a3b0a5ec6c962f99157
Delgorithm <delgorithm@gmail.com>
2024-04-03 09:28:55 +0200

    Feature : Added the Catalogue page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Cart from './pages/Cart'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import User from './pages/User'
+import Catalogue from './components/Catalogue'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={ <Home />} />
         <Route path="/cart" element={ <Cart />} />
         <Route path="/user" element={ <User />} />
+        <Route path='/catalogue' element={ <Catalogue /> } />
       </Routes>
     </Router>
   )
