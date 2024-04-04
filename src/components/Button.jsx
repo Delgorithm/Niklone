b67b2dd440950b284fae07bce6e68a3a651a491b
Delgorithm <delgorithm@gmail.com>
2024-04-04 20:58:18 +0200

    Fix : Some CSS

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({color, label, onClick, text}) => {
   return (
     <button
-      className={`px-2 py-1.5 rounded bg-${color} text-${text} hover:opacity-50 active:translate-y-0.5`}
+      className={`w-full py-1.5 rounded-md bg-${color} text-${text} hover:opacity-50 active:translate-y-0.5`}
       onClick={onClick}
     >
       {label}
