36fb1493d11c238be14236caae9526d6a969cf62
Delgorithm <delgorithm@gmail.com>
2024-04-04 10:22:08 +0200

    Feature : Display the product with its id and started to work on the design

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,20 +3,33 @@ import { useParams } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-const Product = ({ allShoes }) => {
+const Product = ({ allShoes, allSizes }) => {
     const { id } = useParams();
     const currentElement = allShoes.find((element) => element.model === id );
-
+    console.log(allSizes);
     return (
         <>
             <Navbar />
             <section className='m-6'>
-                <h1>{currentElement.model}</h1>
-                <p>{currentElement.type}</p>
-                <p>{currentElement.price}</p>
-                <figure>
-                    <img src={currentElement.img} alt={currentElement.description} />
+                <h1 className='text-xl'>{currentElement.model}</h1>
+                <p className='text-sm'>{currentElement.type}</p>
+                <p>{currentElement.price} €</p>
+                <figure className='my-4'>
+                    <img src={currentElement.img} alt={currentElement.description}/>
                 </figure>
+                <section>
+                    <article className='flex items-center justify-between'>
+                        <p className='font-medium'>Sélectionner la taille</p>
+                        <p className='text-gray-500'>Guide des tailles</p>
+                    </article>
+                    <article className='grid grid-cols-3 grid-row-auto my-4'>
+                        {allSizes.map((size, index) => (
+                            <button key={index} className='m-1.5 px-3 py-2 border-[0.1px] border-gray-300 rounded-lg hover:border-[1px] '>
+                                {size}
+                            </button>
+                        ))}
+                    </article>
+                </section>
             </section>
             <Footer />
         </>
