4349c07807c74b9b5e8257251ae88524d69aaf09
Delgorithm <delgorithm@gmail.com>
2024-04-06 21:48:31 +0200

    Feature : Login/Register page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { NavLink, useNavigate } from "react-router-dom";
+import { auth } from "../api/Firebase/Firebase";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+	const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-export default Login
+	const onLogin = (e) => {
+		e.preventDefault();
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				const user = userCredential.user;
+				navigate("/");
+				console.log(user);
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.log(errorCode, errorMessage);
+			});
+	};
+
+	return (
+		<>
+			<Navbar />
+			<main>
+				<form>
+					<section>
+						<label htmlFor="email-address">Email</label>
+						<input
+							type="email"
+							label="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+							placeholder="Email..."
+						/>
+					</section>
+					<section>
+						<label htmlFor="password">Mot de passe</label>
+						<input
+							type="password"
+							label="Create password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+							placeholder="Mot de passe..."
+						/>
+					</section>
+					<button type="submit" onClick={onLogin}>
+						Se connecter
+					</button>
+				</form>
+				<p>
+					J'ai pas encore de compte: 
+					<NavLink className="	font-bold" to="/register">Se créer un compte</NavLink>
+				</p>
+			</main>
+			<Footer />
+		</>
+	);
+};
+
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	GoogleAuthProvider,
+	signInWithPopup,
+} from "firebase/auth";
+import { NavLink, useNavigate } from "react-router-dom";
+import { auth } from "../api/Firebase/Firebase";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+	const navigate = useNavigate();
+	const provider = new GoogleAuthProvider();
+	const auth = getAuth();
 
-export default Register
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		await createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				const user = userCredential.user;
+				console.log(user);
+				navigate("/");
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.log(errorCode, errorMessage);
+			});
+	};
+
+	const onSubmitGoogleAuth = async (e) => {
+		e.preventDefault();
+		await signInWithPopup(auth, provider)
+			.then((result) => {
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				const token = credential.accessToken;
+				const user = result.user;
+				navigate("/");
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				const email = error.customData.email;
+				const credential = GoogleAuthProvider.credentialFromError(error);
+			});
+	};
+
+	return (
+		<>
+			<Navbar />
+			<main>
+				<form>
+					<section>
+						<label htmlFor="email-address">Email</label>
+						<input
+							type="email"
+							label="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+							placeholder="Email..."
+						/>
+					</section>
+					<section>
+						<label htmlFor="password">Mot de passe</label>
+						<input
+							type="password"
+							label="current-password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+							placeholder="Mot de passe..."
+						/>
+					</section>
+					<button type="submit" onClick={onSubmit}>
+						S'enregistrer
+					</button>
+
+					<section>
+						<p>
+							J'ai déjà un compte <NavLink to="/login">Me connecter</NavLink>
+						</p>
+					</section>
+				</form>
+			</main>
+			<Footer />
+		</>
+	);
+};
+
+export default Register;
