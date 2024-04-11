75da07f6e21be8e3b75f3b4ee46d649a06c52193
Delgorithm <delgorithm@gmail.com>
2024-04-11 18:40:14 +0200

    Fix : Corrected some errors

--- src/context/AuthContext/UserAuthContext.jsx
+++ src/context/AuthContext/UserAuthContext.jsx
@@ -1,42 +1,53 @@
 import React, { createContext, useEffect, useState, useContext } from "react";
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged
-} from "firebase/auth"
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
+	onAuthStateChanged,
+} from "firebase/auth";
 
 import { auth } from "../../api/Firebase/Firebase";
 
 const userAuthContext = createContext();
 
 export function UserAuthContextProvider({ children }) {
-    const [user, setUser] = useState('');
+	const [user, setUser] = useState("");
 
-    function signUp(email, password) {
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
+	function signUp(email, password) {
+		console.log("You sign up successfully");
+		console.log(`Your auth is : ${auth}`);
+		return createUserWithEmailAndPassword(auth, email, password);
+	}
 
-    function logIn(email, password) {
-        return signInWithEmailAndPassword(auth, email, password);
-    }
+	function logIn(email, password) {
+		console.log("You log in successfully");
+		console.log(`Your auth is : ${auth}`);
+		return signInWithEmailAndPassword(auth, email, password);
+	}
 
-    function logOut() {
-        return signOut(auth);
-    }
+	function logOut() {
+		console.log("You log out successfully");
+		console.log(`Your auth is : ${auth}`);
+		return signOut(auth);
+	}
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        });
-        return () => {
-            unsubscribe();
-        }
-    }, []);
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			console.log(`You are the user : ${user}`);
+		});
+		return () => {
+			unsubscribe();
+		};
+	}, []);
 
-	return <userAuthContext.Provider value={{user, signUp, logIn, logOut}}>{children}</userAuthContext.Provider>;
+	return (
+		<userAuthContext.Provider value={{ user, signUp, logIn, logOut }}>
+			{children}
+		</userAuthContext.Provider>
+	);
 }
 
 export function useUserAuth() {
-    return useContext(userAuthContext);
+	return useContext(userAuthContext);
 }
