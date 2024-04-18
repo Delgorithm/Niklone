7b2cad6dd6dccfe690fa534cd70cd0fa09e7d426
Delgorithm <delgorithm@gmail.com>
2024-04-18 07:40:07 +0200

    Feature : New page which display the products added to the cart with some properties

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
-import cartItems from '../assets/data/data.json'
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import { useContext } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const { title, information } = cartItems.cartItems;
+	const { cart } = useContext(CartContext);
 
-  return (
-    <>
-      <Navbar />
-      <section className='m-6'>
-        <p>Cart part</p>
-      </section>
-      <Footer />
-    </>
-  )
-}
+	return (
+		<>
+			<Navbar />
+			<section className="m-6">
+				<h2 className="text-xl mb-4">Mon panier :</h2>
+				{cart.map((item) => {
+					return (
+						<article
+							key={item.id}
+							className="flex items-center justify-between mb-4">
+							<figure>
+								<Link to={`/catalogue/${item.model}`}>
+									<img src={item.img} alt={item.name} className="w-24" />
+								</Link>
+							</figure>
+							<article className="text-end">
+								<p>{item.model}</p>
+								<p>{item.price}€</p>
+							</article>
+						</article>
+					);
+				})}
+			</section>
+			<Footer />
+		</>
+	);
+};
 
-export default Cart
+export default Cart;
