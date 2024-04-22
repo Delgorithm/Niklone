b9a7d14cd459d08b3012777ad10ffee5e29224b6
Delgorithm <delgorithm@gmail.com>
2024-04-22 15:05:40 +0200

    Fix : Changed the CSS for the auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useUserAuth } from "../context/AuthContext/UserAuthContext";
 import SignOut from "./SignOut";
+import Button from "../components/Button";
 
 const SignIn = () => {
 	const [email, setEmail] = useState("");
@@ -28,9 +29,12 @@ const SignIn = () => {
 		<>
 			<Navbar />
 			<main>
-				<form onSubmit={handleSubmit}>
-					<section>
-						<label htmlFor="email-address">Email</label>
+				<h1 className="text-center text-2xl my-4 font-medium">Se connecter</h1>
+				<form onSubmit={handleSubmit} className="mx-6">
+					<section className="flex flex-col">
+						<label htmlFor="email-address" className="text-md font-medium">
+							Email
+						</label>
 						<input
 							type="email"
 							label="Email"
@@ -38,10 +42,13 @@ const SignIn = () => {
 							onChange={(e) => setEmail(e.target.value)}
 							required
 							placeholder="Email..."
+							className="border my-2 p-2 rounded-xl"
 						/>
 					</section>
-					<section>
-						<label htmlFor="password">Mot de passe</label>
+					<section className="flex flex-col">
+						<label htmlFor="password" className="text-md font-medium">
+							Mot de passe
+						</label>
 						<input
 							type="password"
 							label="Create password"
@@ -49,18 +56,30 @@ const SignIn = () => {
 							onChange={(e) => setPassword(e.target.value)}
 							required
 							placeholder="Mot de passe..."
+							className="border my-2 p-2 rounded-xl"
 						/>
 					</section>
-					<button type="submit" onClick={handleSubmit}>
-						Se connecter
-					</button>
+					<article className="flex justify-center py-5">
+						<Button
+							label="Se connecter"
+							color="black"
+							text="white"
+							onClick={handleSubmit}
+							type="submit"
+						/>
+					</article>
 				</form>
-				<p>
-					J'ai pas encore de compte:
-					<NavLink className="font-medium" to="/signup">
-						Se créer un compte
-					</NavLink>
-				</p>
+				<section className="flex justify-around items-center mx-6 text-center">
+					<div className="w-16 h-[0.5px] bg-black"></div>
+					<small className="flex flex-col text-xs">
+						J'ai pas encore de compte
+						<NavLink className="text-sm font-medium underline" to="/signup">
+							Se créer un compte
+						</NavLink>
+					</small>
+					<div className="w-16 h-[0.5px] bg-black"></div>
+					<span></span>
+				</section>
 				<SignOut />
 			</main>
 			<Footer />
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,6 +3,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useUserAuth } from "../context/AuthContext/UserAuthContext";
+import Button from "../components/Button";
 
 const SignUp = () => {
 	const [email, setEmail] = useState("");
@@ -27,9 +28,14 @@ const SignUp = () => {
 		<>
 			<Navbar />
 			<main>
-				<form onSubmit={handleSubmit}>
-					<section>
-						<label htmlFor="email-address">Email</label>
+				<h1 className="text-center text-2xl my-4 font-medium">
+					Se créer un compte
+				</h1>
+				<form onSubmit={handleSubmit} className="mx-6">
+					<section className="flex flex-col">
+						<label htmlFor="email-address" className="text-md font-medium">
+							Email
+						</label>
 						<input
 							type="email"
 							label="Email"
@@ -37,10 +43,13 @@ const SignUp = () => {
 							onChange={(e) => setEmail(e.target.value)}
 							required
 							placeholder="Email..."
+							className="border my-2 p-2 rounded-xl"
 						/>
 					</section>
-					<section>
-						<label htmlFor="password">Mot de passe</label>
+					<section className="flex flex-col">
+						<label htmlFor="password" className="text-md font-medium">
+							Mot de passe
+						</label>
 						<input
 							type="password"
 							label="current-password"
@@ -48,19 +57,27 @@ const SignUp = () => {
 							onChange={(e) => setPassword(e.target.value)}
 							required
 							placeholder="Mot de passe..."
+							className="border my-2 p-2 rounded-xl"
 						/>
 					</section>
-					<button type="submit" onClick={handleSubmit}>
-						S'enregistrer
-					</button>
-
-					<section>
-						<p>
-							J'ai déjà un compte{" "}
-							<NavLink className="font-medium" to="/signin">
-								Me connecter
+					<article className="flex justify-center py-5">
+						<Button
+							label="Se connecter"
+							color="black"
+							text="white"
+							onClick={handleSubmit}
+							type="submit"
+						/>
+					</article>
+					<section className="flex justify-around items-center text-center mx-6 pb-10">
+						<div className="w-16 h-[0.5px] bg-black"></div>
+						<small className="flex flex-col text-xs">
+							J'ai déjà un compte
+							<NavLink className="text-sm font-medium underline" to="/signin">
+								Se connecter
 							</NavLink>
-						</p>
+						</small>
+						<div className="w-16 h-[0.5px] bg-black"></div>
 					</section>
 				</form>
 			</main>
