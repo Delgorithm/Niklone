b8393daeaa6498c891dbf072af2c93fce5ffba8a
Delgorithm <delgorithm@gmail.com>
2024-05-04 14:16:26 +0200

    Changed some path

--- src/App.jsx
+++ src/App.jsx
@@ -12,44 +12,47 @@ import "./App.css";
 import User from "./pages/User";
 import ProtectedRoute from "./components/ProtectedRoute";
 import CartProvider from "./context/CartContext";
+import { FavoriteProvider } from "./context/FavoriteContext";
 
 function App() {
 	const { catalogueShoes, sizes, detailProduct, detailReview } = data;
 
 	const allShoes = Object.values(catalogueShoes);
 	const allSizes = Object.values(sizes);
-	const allDetails = Object.values(detailProduct);
-	const allReview = Object.values(detailReview);
+	// const allDetails = Object.values(detailProduct);
+	// const allReview = Object.values(detailReview);
 
 	return (
 		<UserAuthContextProvider>
-			<CartProvider>
-				<Router>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/cart" element={<Cart />} />
-						<Route path="/newsletter" element={<Newsletter />} />
-						<Route path="/signin" element={<SignIn />} />
-						<Route path="/signup" element={<SignUp />} />
-						<Route
-							path="/catalogue"
-							element={<Catalogue allShoes={allShoes} />}
-						/>
-						<Route
-							path="/catalogue/:id"
-							element={<Product allShoes={allShoes} allSizes={allSizes} />}
-						/>
-						<Route
-							path="/user"
-							element={
-								<ProtectedRoute>
-									<User />
-								</ProtectedRoute>
-							}
-						/>
-					</Routes>
-				</Router>
-			</CartProvider>
+			<FavoriteProvider>
+				<CartProvider>
+					<Router>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/cart" element={<Cart />} />
+							<Route path="/newsletter" element={<Newsletter />} />
+							<Route path="/signin" element={<SignIn />} />
+							<Route path="/signup" element={<SignUp />} />
+							<Route
+								path="/catalogue"
+								element={<Catalogue allShoes={allShoes} />}
+							/>
+							<Route
+								path="/catalogue/:id"
+								element={<Product allShoes={allShoes} allSizes={allSizes} />}
+							/>
+							<Route
+								path="/user"
+								element={
+									<ProtectedRoute>
+										<User />
+									</ProtectedRoute>
+								}
+							/>
+						</Routes>
+					</Router>
+				</CartProvider>
+			</FavoriteProvider>
 		</UserAuthContextProvider>
 	);
 }
