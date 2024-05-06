ef655ddc25a801f13c7dd9de1bfc68f81d21cb50
Delgorithm <delgorithm@gmail.com>
2024-05-06 11:42:52 +0200

    updated for the deployment on vercel

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
 	cors({
-		origin: ["https://niklone.vercel.app"],
+		origin: ["https://niklone.vercel.app/"],
 		methods: ["GET", "POST"],
 		credentials: true,
 	})
