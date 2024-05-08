cefc080efb8836ae77fd2534c85798d5b1fd4cd7
Delgorithm <delgorithm@gmail.com>
2024-05-08 15:53:09 +0200

    Fix : Revert to the initial one which works on localhost

--- server/server.js
+++ server/server.js
@@ -1,22 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+let cors = require("cors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const PORT = process.env.PORT || 4000;
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("Hello world from backend!");
-});
-
 app.post("/checkout", async (req, res) => {
-	console.log("Requête POST reçue sur /checkout :", req.body);
+	console.log(req.body);
 	const items = req.body.items;
 
 	let lineItems = [];
@@ -27,35 +20,18 @@ app.post("/checkout", async (req, res) => {
 		});
 	});
 
-	try {
-		const session = await stripe.checkout.sessions.create({
-			line_items: lineItems,
-			mode: "payment",
-			success_url: `${process.env.VERCEL_URL}/success`,
-			cancel_url: `${process.env.VERCEL_URL}/cancel`,
-		});
+	const session = await stripe.checkout.sessions.create({
+		line_items: lineItems,
+		mode: "payment",
+		success_url: "http://localhost:5173/success",
+		cancel_url: "http://localhost:5173/cancel",
+	});
 
-		console.log("Session créée avec succès :", session);
-
-		res.send(
-			JSON.stringify({
-				url: session.url,
-			})
-		);
-	} catch (error) {
-		console.error(
-			"Une erreur s'est produite lors de la création de la session :",
-			error
-		);
-
-		res.status(500).json({
-			error: "Une erreur s'est produite lors du traitement de la requête.",
-		});
-	}
+	res.send(
+		JSON.stringify({
+			url: session.url,
+		})
+	);
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-	console.log(`Serveur en cours d'écoute sur le port ${PORT}`);
-});
-
-module.exports = app;
+app.listen(4000, () => console.log("Listening on port 4000"));
